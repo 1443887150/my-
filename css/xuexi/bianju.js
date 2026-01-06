d507a31c288defd86ffbb7b1e51cf006312ef9ff
1443887150 <1443887150@qq.com>
2026-01-06 23:40:14 +0800

    Add files via upload

---
+++ css/xuexi/bianju.js
@@ -0,0 +1,33 @@
+var jf=26
+var zz=false
+var jf1=10,jf2=20,jf3=30
+// console.log(jf)
+// document.write(jf)
+// console.log(typeof jf)
+if(jf>20&&jf<30){
+    console.log("青年")
+}
+if(jf>=30){
+    console.log("中老年")
+}
+if(!zz){
+    console.log("人")
+}
+console.log(jf1?jf2:jf3)
+var aa="   nihao,wojiaojifan   "
+a=aa.length
+// for(i=0;i<=a-1;i++){
+//     console.log(aa.charAt(i))
+// }
+b=aa.indexOf("o",5)
+console.log(aa)
+c=aa.trim()
+console.log(c)
+d=aa.split("")
+console.log(d)
+aaa=20
+console.log(aaa)
+var cd=["jf","zyx"]
+cd.push("er")
+cd.pop(2)
+console.log(cd)
